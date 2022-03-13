866fd3c09525de39d0cde4fdedf185052d8a4f02
Ali Abas Arsalahn <ali.abas.arsalahn@icloud.com>
2022-03-13 21:11:17 +0100

    added prompt getter and setter. Changed __init__ method to include self.prompt without passing it as argument

--- interface.py
+++ interface.py
@@ -5,18 +5,50 @@
 '''Interface module'''
 
 
-class CLIInterface:
-    '''class CLI.\n Template for buidling an interactive interface.'''
-    def __init__(self, page: int, display: str, interaction: list):
-        self.page = page
-        self._display = display
-        self.interaction = interaction
+class SNCLI:
+    '''class CLI.\n Template for building an interactive interface.'''
+
+    def __init__(self, page: int):
+        self._page = page
+        self.interaction = None
+        self.prompt = None
+
+    @property
+    def page(self):
+        '''page getter.'''
+        return self._page
+
+    @page.setter
+    def page(self, value) -> int:
+        '''page setter'''
+        if value >= 1:
+            self._page = value
+
+    @property
+    def interaction(self):
+        """returns the interaction property"""
+        return self.interaction
+
+    @interaction.setter
+    def interaction(self) -> None:
+        """sets the available interactions based on page"""
+        self.interaction = (1, 2, 3) if self._page == 1 else (1, 2)
 
     @property
     def prompt(self):
-        '''Display getter.'''
-        return self._display
+        """returns prompt property"""
+        return self.prompt
 
     @prompt.setter
-    def prompt(self, text: str) -> None:
-        self._display = text
+    def prompt(self) -> str:
+        """sets the prompt attribute based on page number"""
+        main_menu_prompt = """Welcome to SN-GEN!
+        1 - generate serialnumber
+        2 - save serialnumber
+        3 - exit."""
+        generator_prompt = """SN-GEN:
+        1 - to save your generated serialnumbers
+        2 - back to main menu
+        3 - exit."""
+
+        self._prompt = main_menu_prompt if self._page == 1 else generator_prompt
