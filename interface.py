28684b16ceb7a0ebf7c70e354a2e6d77350f4c4e
Ali Abas Arsalahn <ali.abas.arsalahn@icloud.com>
2022-03-14 16:26:47 +0100

    restruction of class SNCLI

--- interface.py
+++ interface.py
@@ -11,34 +11,9 @@ class SNCLI:
     def __init__(self):
         """initialize an object of the class SNCLI. Page, interactions and
         prompt are set automatically"""
-        self._page = None
-        self.interactions = {}
-        self.prompt = ''
+        self.page = None
+        self.interface = {}
 
-    @page.setter
-    def page(self, value) -> int:
-        '''page setter'''
-        if value >= 1:
-            self._page = value
-
-    @property
-    def interaction(self):
-        """returns the interaction property"""
-        return self.interaction
-
-    @interaction.setter
-    def interaction(self) -> None:
-        """sets the available interactions based on page"""
-        self.interaction = (1, 2, 3) if self._page == 1 else (1, 2)
-
-    @property
-    def prompt(self):
-        """returns prompt property"""
-        return self.prompt
-
-    @prompt.setter
-    def prompt(self) -> str:
-        """sets the prompt attribute based on page number"""
         main_menu_prompt = """Welcome to SN-GEN!
         1 - generate serialnumber
         2 - save serialnumber
@@ -49,11 +24,12 @@ class SNCLI:
         2 - back to main menu
         3 - exit."""
 
-        self._prompt = main_menu_prompt if self._page == 1 else generator_prompt
-
     def run(self) -> None:
         """starts the programm loop and takes user input."""
         while True:
             user_interaction = input(self.prompt)
 
-            if user_interaction == 
+            try:
+                print(self.interface[user_interaction])
+            except IndexError:
+                print("command not found!")
