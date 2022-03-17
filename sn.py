b40a022962c2502b57f672e41b624872c1a01bfc
Ali Abas Arsalahn <ali.abas.arsalahn@icloud.com>
2022-03-17 22:25:52 +0100

    removed properties from the __init__ Method and made them to arguments of the method key_generator

--- sn.py
+++ sn.py
@@ -2,7 +2,7 @@
 # Author: Ali Abas Arsalahn
 # Datum: 12.03.2022
 """
-sn: class definition for sn-project
+SN class module.
 """
 
 
@@ -16,43 +16,50 @@ class SN:
     key gets created according to set paramaters of the object.\n
     sn-objects get created with Valid flag on default.
     """
-    def __init__(self, sn_type: str, rows: int, row_length: int):
-        self.type = sn_type
-        self.rows = rows
-        self.row_length = row_length
-        self.valid = True
+
+    def __init__(self):
+        """Creates a class objekt and assigns the constant TYPES as property."""
         self.TYPES = ['letter', 'digit', 'mixed']
+        self.keys = {}
 
-    def key_generator(self) -> str:
+    def key_generator(self, type: str, rows: int, row_length: int) -> None:
         """
-        Checks the type of the object and returns a random character from
-        the Strings: string.ascii_letters, string.digits or both.
+        Takes type, rows and row_length as arguments.
+        Calls the function available_characters to get a random character.
+        Generates the key-string and puts it in the dictionary "keys".
         """
-        #####################WORK-IN-PROGRESS#################################
-        key = ''
-        if self.type == 'letter':
-            rnd_nmb = randrange((len(ascii_letters)) - 1)
-            return ascii_letters[rnd_nmb]
-        elif self.type == 'digit':
-            rnd_nmb = randrange((len(digits)) - 1)
-            return digits[rnd_nmb]
-        else:
-            rnd_nmb = randrange((len(ascii_letters + digits)) - 1)
-            return (ascii_letters + digits)[rnd_nmb]
+        def available_characters(type: str) -> str:
+            """
+            Checks the type of the object and returns a random character from
+            the Strings: string.ascii_letters, string.digits or both.
+            """
+            if type == 'letter':
+                rnd_nmb = randrange((len(ascii_letters)) - 1)
+                return ascii_letters[rnd_nmb]
+            elif type == 'digit':
+                rnd_nmb = randrange((len(digits)) - 1)
+                return digits[rnd_nmb]
+            else:
+                rnd_nmb = randrange((len(ascii_letters + digits)) - 1)
+                return (ascii_letters + digits)[rnd_nmb]
 
-        for i in range(self.rows * self._row_length):
-            key += key_generator(self.type)
-            if (i >= self._row_length) and (i / self._row_length == 1):
+        key: str = ''
+        for i in range(rows * row_length):
+            key.join(available_characters(type))
+            if (i >= row_length) and (i / row_length == 1):
                 key += '-'
+        self.keys[key[:-1]] = True
 
     def save_serialnumber(self) -> None:
         """
-        Takes a created SN as argument and appends it to a CSV file.
+        Takes the dictionary "keys" and writes them to a file.
         """
-        pass
+        with open('keys.csv', 'w') as file:
+            pass
 
     def validate_serialnumber(self) -> None:
         """
         Takes a SN as an argument and checks if it is valid.
         """
-        pass
+        with open('keys.csv', 'w') as file:
+            pass
