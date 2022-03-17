465906e0c0b6a9a5ab912a96a4529398a34bd479
Ali Abas Arsalahn <ali.abas.arsalahn@icloud.com>
2022-03-17 04:36:35 +0100

    Restructing... WORK-IN-PROGRESS

--- interface.py
+++ interface.py
@@ -1,47 +1,59 @@
 # Projekt: Seriennummergenerator | interface
 # Author: Ali Abas Arsalahn
 # Datum: 12.03.2022
-'''Interface module'''
+"""
+Interface module
+"""
 
 
 class SNCLI:
-    '''class CLI.\n Template for building an interactive interface.'''
+    '''
+    Template for SNGEN CLI
+    '''
+    # ggf. implicit setzen
+    def __init__(self):
+        """
+        constructer of class SNCLI.
+        """
+        pass
 
-    def __init__(self, page: int):
-        """initialize an object of the class SNCLI. Page, interactions and
-        prompt are set automatically"""
-        self._page = page
-        self._interface = interface
-
-        @property
-        def interface(self) -> dict:
-            return self._interface
-
-        @interface.setter
-        def interface(self):
-            """sets interface propterty."""
-            ###########################SUBJECT-TO-CHANGE######################
-            main_page = """Welcome to SN-GEN!
+    def main_menu(self) -> None:
+        """
+        starts the main menu.
+        Assigns the prompt as a string and displays it.
+        Prompt displays
+        Takes a command as an argument afterwards.
+        """
+        main_menu_prompt = """
+                    Welcome to SN-GEN!
                     1 - generate serialnumber
                     2 - save serialnumber
                     3 - validate serialnumber
-                    4 - exit."""
-            generator_prompt = """1 - to save your generated serialnumbers
-        2 - back to main menu
-        3 - exit."""
-            validate_prompt = """something"""
-            self.interface = {
-                1: main_page,
-                2: generator_prompt,
-                3: validate_prompt
-            }
+                    4 - exit.
+                    """
+        main_menu_commands = {
+            # function calls
+            #
+        }
+        command = input(main_menu_prompt)
+
+    def generate_serialnumber(self) -> None:
+        """
+        Displays the Serialnumber context menu and takes an argument.
+        """
+        generate_serialnumber_prompt = """
+                                    1 - generate Serialnumber
+                                    2 - change settings
+                                    3 - save serialnumber
+                                    """
+        generate_serialnumber_commands = {
+            # commands
+        }
+
+        command = input(generate_serialnumber_prompt)
 
     def run(self) -> None:
-        """starts the programm loop and takes user input."""
-        while True:
-            user_interaction = input(self._interface[self._page])
-
-            try:
-                print(self._interface[user_interaction])
-            except IndexError:
-                print("command not found!")
+        """
+        starts the programm loop and takes user input.
+        """
+#        while True:
--- sn.py
+++ sn.py
@@ -1,7 +1,9 @@
 # Projekt: Seriennummergenerator | Modul: sn
 # Author: Ali Abas Arsalahn
 # Datum: 12.03.2022
-'''sn: class definition for sn-project'''
+"""
+sn: class definition for sn-project
+"""
 
 
 from string import ascii_letters, digits
@@ -9,58 +11,48 @@ from random import randrange
 
 
 class SN:
-    '''
+    """
     sn-object can be iniatilized with varying type, rows and rowlength.\n
     key gets created according to set paramaters of the object.\n
-    sn-objects get created with Valid flag on default.'''
-
-    TYPES = ['letter', 'digit', 'mixed']
-
-    def __init__(self, sn_type, rows: int, row_length: int, key='', valid=True):
-        self._type = sn_type
+    sn-objects get created with Valid flag on default.
+    """
+    def __init__(self, sn_type: str, rows: int, row_length: int):
+        self.type = sn_type
         self.rows = rows
-        self._row_length = row_length
-        self.valid = valid
-        self._key = key
+        self.row_length = row_length
+        self.valid = True
+        self.TYPES = ['letter', 'digit', 'mixed']
 
-    def __str__(self) -> str:
-        return f"type: {self._type}, rows: {self.rows}, valid: {self.valid}"
-
-    @property
-    def sn_type(self):
-        '''type getter'''
-        return self._type
-
-    @sn_type.setter
-    def sn_type(self, value) -> None:
-        '''Setter for type property.\n
-        User can choose between set types that are defined in the CONST List TYPES.\n'''
-        if value in self.TYPES:
-            self._type = value
-
-    @property
-    def key(self):
-        '''key getter'''
-        return self._key
-
-    @key.setter
-    def key(self) -> str:
-        self._key = ''
-
-        def char_generator(type: str) -> str:
-            '''Checks the type of the object and returns a random character from
-            the Strings: string.ascii_letters, string.digits or both.'''
-            if type == 'letter':
-                rnd_nmb = randrange((len(ascii_letters)) - 1)
-                return ascii_letters[rnd_nmb]
-            elif type == 'digit':
-                rnd_nmb = randrange((len(digits)) - 1)
-                return digits[rnd_nmb]
-            else:
-                rnd_nmb = randrange((len(ascii_letters + digits)) - 1)
-                return (ascii_letters + digits)[rnd_nmb]
+    def key_generator(self) -> str:
+        """
+        Checks the type of the object and returns a random character from
+        the Strings: string.ascii_letters, string.digits or both.
+        """
+        #####################WORK-IN-PROGRESS#################################
+        key = ''
+        if self.type == 'letter':
+            rnd_nmb = randrange((len(ascii_letters)) - 1)
+            return ascii_letters[rnd_nmb]
+        elif self.type == 'digit':
+            rnd_nmb = randrange((len(digits)) - 1)
+            return digits[rnd_nmb]
+        else:
+            rnd_nmb = randrange((len(ascii_letters + digits)) - 1)
+            return (ascii_letters + digits)[rnd_nmb]
 
         for i in range(self.rows * self._row_length):
-            self._key += char_generator(self._type)
+            key += key_generator(self.type)
             if (i >= self._row_length) and (i / self._row_length == 1):
-                self._key += '-'
+                key += '-'
+
+    def save_serialnumber(self) -> None:
+        """
+        Takes a created SN as argument and appends it to a CSV file.
+        """
+        pass
+
+    def validate_serialnumber(self) -> None:
+        """
+        Takes a SN as an argument and checks if it is valid.
+        """
+        pass
