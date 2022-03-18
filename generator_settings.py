20b3a953cae6d55aef0b29e09b846e9ef87be68a
Ali Abas Arsalahn <ali.abas.arsalahn@icloud.com>
2022-03-18 17:25:03 +0100

    t# Please enter the commit message for your changes. Lines starting

---
+++ generator_settings.py
@@ -0,0 +1,17 @@
+# Projekt Seriennummergenerator | Modul: generator_settings
+# Author: Ali Abas Arsalahn
+# Datum: 18.03.2022
+"""
+settings module
+"""
+
+
+from sn import SN
+from interface import SNCLI
+
+
+class settings:
+    """settings class"""
+    
+    def __init__(self) -> None:
+        pass
--- sn.py
+++ sn.py
@@ -8,58 +8,56 @@ SN class module.
 
 from string import ascii_letters, digits
 from random import randrange
+import json
 
-
-class SN:
+class SN():
     """
-    sn-object can be iniatilized with varying type, rows and rowlength.\n
-    key gets created according to set paramaters of the object.\n
-    sn-objects get created with Valid flag on default.
+    sn-object can be iniatilized as number, letter or mixed generator.
     """
 
-    def __init__(self):
-        """Creates a class objekt and assigns the constant TYPES as property."""
-        self.TYPES = ['letter', 'digit', 'mixed']
+    def __init__(self, type):
         self.keys = {}
+        self.type = type
 
-    def key_generator(self, type: str, rows: int, row_length: int) -> None:
+    def key_generator(self, count: int,rows: int, row_length: int) -> None:
         """
         Takes type, rows and row_length as arguments.
         Calls the function available_characters to get a random character.
         Generates the key-string and puts it in the dictionary "keys".
         """
-        def available_characters(type: str) -> str:
+        def generate_char(type) -> str:
             """
             Checks the type of the object and returns a random character from
-            the Strings: string.ascii_letters, string.digits or both.
+            the Strings: string.ascii_letters, string.digits.
             """
             if type == 'letter':
                 rnd_nmb = randrange((len(ascii_letters)) - 1)
                 return ascii_letters[rnd_nmb]
-            elif type == 'digit':
+            else:
                 rnd_nmb = randrange((len(digits)) - 1)
                 return digits[rnd_nmb]
-            else:
-                rnd_nmb = randrange((len(ascii_letters + digits)) - 1)
-                return (ascii_letters + digits)[rnd_nmb]
 
         key: str = ''
-        for i in range(rows * row_length):
-            key.join(available_characters(type))
-            if (i >= row_length) and (i / row_length == 1):
-                key += '-'
-        self.keys[key[:-1]] = True
+        for i in range(count + 1):
+            for i in range(rows * row_length):
+                key.join(generate_char(self.type))
+                if (i >= row_length) and (i / row_length == 1):
+                    key += '-'
+            self.keys[key[:-1]] = True
 
     def save_serialnumber(self) -> None:
         """
         Takes the dictionary "keys" and writes them to a file.
         """
-        with open('keys.csv', 'w') as file:
-            pass
+        with open('keys.json', 'a') as file:
+            json.dump(self.keys, file)
+
+    def delete_serialnumber(self) -> None:
+        pass
 
     def validate_serialnumber(self) -> None:
         """
         Takes a SN as an argument and checks if it is valid.
         """
-        with open('keys.csv', 'w') as file:
+        with open('keys.json', 'r') as file:
             pass
