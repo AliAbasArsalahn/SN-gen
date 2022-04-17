f868c2d0487a066688a3be85eb7469f85b77b558
Ali Abas Arsalahn <ali.abas.arsalahn@icloud.com>
2022-04-17 15:45:35 +0200

    -/-

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-# Projekt: Seriennummergenerator | Modul: argparse
+# Projekt: Seriennummergenerator | Modul: main
 # Author: Ali Abas Arsalahn
 # Datum: 12.03.2022
 """main module"""
 
 import argparse
-from sn import LetterGenerator, DigitGenerator
+from generator import LetterGenerator, DigitGenerator
 
 
 def generate_and_save(thing: object) -> None:
@@ -18,12 +18,19 @@ def validate(thing: object) -> None:
     thing.validate_serialnumber()
 
 
+parser = argparse.ArgumentParser(description="generates a serialnumber")
+parser.add_argument("-gen", "--generator", type=str, choices=["letter", "digit"],
+                        help="specify the type of serialnumber")
+parser.add_argument("-qt", "--quantity", type=int,
+                        help="set the amount of generated serialnumbers")
+parser.add_argument("-p", "--path", type=str, help="set the path")
+
+args = vars(parser.parse_args())
+generator = LetterGenerator() if args[generator] == "letter" else DigitGenerator()
+
 def main() -> None:
     """main function"""
-    parser = argparse.ArgumentParser(description="generates a serialnumber")
-    parser.add_argument("-gen", "--generate", type=str, choices=["letter", "digit"],
-                        help="specify the type of serialnumber")
-    args = parser.parse_args()
+    generate_and_save(generator)
 
 
 if __name__ == '__main__':
