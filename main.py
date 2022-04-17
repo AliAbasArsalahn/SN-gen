e2d4f03daba279b5e00635d56f6ea85527733d0b
Ali Abas Arsalahn <ali.abas.arsalahn@icloud.com>
2022-04-17 22:08:26 +0200

    renamed test_sn.py -> test_generator.py

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ import argparse
 from generator import LetterGenerator, DigitGenerator
 
 
-def generate_and_save(thing: object) -> None:
-    """it's a duck!"""
-    thing.generate_serialnumber()
-    thing.save_serialnumber()
+# def generate_and_save(thing: object) -> None:
+#     """it's a duck!"""
+#     thing.generate_serialnumber()
+#     thing.save_serialnumber()
+
+
+generate = lambda thing: thing.generate_serialnumber()
+save = lambda thing: thing.save_serialnumber()
 
 
 def validate(thing: object) -> None:
@@ -18,29 +22,20 @@ def validate(thing: object) -> None:
     thing.validate_serialnumber()
 
 
-# parser = argparse.ArgumentParser(description="generates a serialnumber")
-# parser.add_argument("-gen", "--generator", type=str, choices=["letter", "digit"],
-#                         metavar="gen" ,help="specify the type of serialnumber")
-# parser.add_argument("-qt", "--quantity", type=int,
-#                         help="set the amount of generated serialnumbers")
-# parser.add_argument("-p", "--path", type=str, help="set the path")
-
-# args = vars(parser.parse_args())
-# generator = LetterGenerator() if args["generator"] == "letter" else DigitGenerator()
-
-
 def main() -> None:
     """main function"""
     parser = argparse.ArgumentParser(description="generates a serialnumber")
     parser.add_argument("-gen", "--generator", type=str, choices=["letter", "digit"],
-                        metavar="gen" ,help="specify the type of serialnumber")
+                        metavar="gen", help="specify the type of serialnumber")
     parser.add_argument("-qt", "--quantity", type=int,
                         help="set the amount of generated serialnumbers")
     parser.add_argument("-p", "--path", type=str, help="set the path")
 
     args = vars(parser.parse_args())
-    generator = LetterGenerator() if args["generator"] == "letter" else DigitGenerator()
-    generate_and_save(generator)
+    generator = LetterGenerator(
+    ) if args["generator"] == "letter" else DigitGenerator()
+    generate(generator)
+    save(generator)
 
 
 if __name__ == '__main__':
